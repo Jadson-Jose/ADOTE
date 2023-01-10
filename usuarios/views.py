52f6f1d7e10162786c61136125c13d93e9283c4f
Jadson José da Silva <69395306+Jadson-Jose@users.noreply.github.com>
2023-01-09 22:17:34 -0300

    sinc

--- usuarios/views.py
+++ usuarios/views.py
@@ -5,4 +5,9 @@ def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
     elif request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        print(nome, email, senha, confirmar_senha)
         return HttpResponse('Teste')
